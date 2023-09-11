fa6c12096efd4d1d90b78d1b207ba4da6d23c0fe
friedrichweise <account@weise.io>
2023-09-11 11:52:14 +0200

    improves initial db migration to match mvp features

--- laravel/app/Enums/IngredientState.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-enum IngredientState: string
-{
-    case DRAFT = 'draft';
-    case REVIEWED = 'reviewed';
-    case PUBLISHED = 'published';
-}
-?>
--- laravel/database/migrations/2023_08_02_101323_create_initial_cowiki_structure.php
+++ laravel/database/migrations/2023_08_02_101323_create_initial_cowiki_structure.php
@@ -10,41 +10,43 @@ return new class () extends Migration {
      */
     public function up(): void
     {
+
+        Schema::create('licenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('description')->nullable();
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+
         Schema::create('ingredients', function (Blueprint $table) {
             $table->id();
-            $table->string('title',255);
-            $table->string('slug',255)->unique();
+            $table->string('title', 255);
+            $table->string('slug', 255)->unique();
             $table->text('description');
             $table->string('filename', 255);
             $table->text('source');
-            /** ENUM */
-            $table->string('status');
-            /** end ENUM */
-            $table->string('license_id');
+
+            $table->foreignId('license_id')->nullable();
+            $table->foreign('license_id')->references('id')->on('licenses');
+
             $table->timestamps();
         });
 
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
-            $table->string('title',255);
-            $table->string('slug',255)->unique();
+            $table->string('title', 255);
+            $table->string('slug', 255)->unique();
             $table->text('description')->nullable();
             $table->timestamps();
         });
 
         Schema::create('collections', function (Blueprint $table) {
             $table->id();
-            $table->string('title',255);
-            $table->string('slug',255)->unique();
+            $table->string('title', 255);
+            $table->string('slug', 255)->unique();
             $table->text('description')->nullable();
             $table->timestamps();
-        });
-
-        Schema::create('collection_recipe', function (Blueprint $table) {
-            $table->foreignId('collection_id');
-            $table->foreignId('recipe_id');
-            $table->foreign('collection_id')->references('id')->on('collections');
-            $table->foreign('recipe_id')->references('id')->on('recipes');
         });
 
         Schema::create('ingredient_recipe', function (Blueprint $table) {
@@ -54,17 +56,11 @@ return new class () extends Migration {
             $table->foreign('recipe_id')->references('id')->on('recipes');
         });
 
-        Schema::create('licenses', function (Blueprint $table) {
-            $table->id();
-            $table->string('title',255);
-            $table->string('icon_string',255)->unique();
-            $table->text('description')->nullable();
-            $table->boolean('active')->default(false);
-            $table->timestamps();
-        });
-
-        Schema::table('ingredients', function (Blueprint $table) {
-            $table->foreign('license_id')->references('id')->on('licenses');
+        Schema::create('collection_recipe', function (Blueprint $table) {
+            $table->foreignId('collection_id');
+            $table->foreignId('recipe_id');
+            $table->foreign('collection_id')->references('id')->on('collections');
+            $table->foreign('recipe_id')->references('id')->on('recipes');
         });
     }
 
