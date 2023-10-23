58d7f5fdee68c0cdeef67831a7463b6919e31cce
friedrichweise <account@weise.io>
2023-10-23 09:47:02 +0000

    adds toast based notficiations #9

--- vue/src/plugins/api.js
+++ vue/src/plugins/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
-
+import { useToast } from 'vue-toastification'
 const API_URL = import.meta.env.VITE_API_ROOT + 'api/'
 
 const api = axios.create({
   baseURL: API_URL,
 })
+
+const toast = useToast()
 
 export const makeApiRequest = config => {
   return api(config)
@@ -19,15 +21,19 @@ export const makeApiRequest = config => {
         return response.data.data
       } else {
         throw new Error('data property does not exist in the response body')
+        toast.error('data property does not exist in the response body')
       }
     })
     .catch(error => {
       if (error.response) {
         console.error(`Server error with code ${error.response.status}`)
+        toast.error(`Server error with code ${error.response.status}`)
       } else if (error.request) {
         console.error(`Request error ${error.request}`)
+        toast.error(`Request error ${error.request}`)
       } else {
         console.error(`Error: ${error.message}`)
+        toast.error(`Error: ${error.message}`)
       }
       throw error
     })
