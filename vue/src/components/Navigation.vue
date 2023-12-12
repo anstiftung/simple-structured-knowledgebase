120dfe6a9bb7747796c526c0b45b82882f943884
makakken <post@makakken.de>
2023-12-12 09:53:04 +0100

    auth is now in pinia store

--- vue/src/components/Navigation.vue
+++ vue/src/components/Navigation.vue
@@ -7,7 +7,6 @@ const callLogin = () => {
 }
 
 const userStore = useUserStore()
-console.log(userStore.hasToken)
 </script>
 
 <template>
@@ -19,7 +18,15 @@ console.log(userStore.hasToken)
           <span class="font-bold">COWIKI</span>
         </router-link>
       </h1>
-      <a href="#" @click.prevent="callLogin">
+      <template v-if="userStore.hasToken">
+        <router-link to="/dashboard">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256">
+                <path d="M208,80H96V56a32,32,0,0,1,32-32c15.37,0,29.2,11,32.16,25.59a8,8,0,0,0,15.68-3.18C171.32,24.15,151.2,8,128,8A48.05,48.05,0,0,0,80,56V80H48A16,16,0,0,0,32,96V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V96A16,16,0,0,0,208,80Zm0,128H48V96H208V208Z"></path>
+            </svg>
+        </router-link>
+      </template>
+      <template v-else>
+        <a href="#" @click.prevent="callLogin">
         <svg
           class="fill-gray-800"
           xmlns="http://www.w3.org/2000/svg"
@@ -32,6 +39,7 @@ console.log(userStore.hasToken)
           />
         </svg>
       </a>
+      </template>
     </div>
   </header>
 </template>
--- vue/src/stores/user.js
+++ vue/src/stores/user.js
@@ -1,20 +1,23 @@
+import { useLocalStorage } from "@vueuse/core"
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', {
     state: () => ({
-        token: window.localStorage.getItem('keycloakToken') ?? undefined
+        token: useLocalStorage('keycloakToken', null)
     }),
     getters: {
-      hasToken: (state) => { state.token !== undefined ? true : false },
+      hasToken: (state) => { return state.token !== null ? true : false },
     },
     actions: {
       setToken(token) {
         this.token = token
         window.localStorage.setItem('keycloakToken', token)
+        console.log('set token...' + token)
       },
       removeToken() {
         this.token = undefined
         window.localStorage.removeItem('keycloakToken')
+        console.log('remove token...')
       },
     },
 })
