0e6d6df602eb91b5dfd1e14add898448b617dfe6
makakken <post@makakken.de>
2023-12-14 11:46:21 +0100

    fixed redirect after login

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -11,21 +11,17 @@ router.beforeEach((to, from, next) => {
     const $keycloak = inject('keycloak')
     if (to.meta.protected) {
       // Get the actual url of the app, it's needed for Keycloak
-      console.log('protected...')
       const basePath = window.location.origin.toString()
       if (!$keycloak.authenticated) {
         // The page is protected and the user is not authenticated. Force a login.
-        console.log('not authenticated')
 
-        $keycloak.login({onLoad: 'login-required', checkLoginIframe: false}).then(() => {
-            console.log('redirect to next....')
+        $keycloak.login({redirectUri: basePath + to.path}).then(() => {
             next()
         }).catch( err => {
             console.error(err)
             next({ name: 'not-authorized' })
         })
       } else {
-        console.log('not authenticated')
         next()
       }
         // $keycloak.updateToken(70)
