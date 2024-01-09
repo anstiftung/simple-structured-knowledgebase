7d1a9f8b0d7ee3e976414414f55b975d10f59de1
makakken <post@makakken.de>
2024-01-09 17:22:59 +0100

    download attached files from old-cowiki and update attachedFiles metadata

---
+++ laravel/app/Console/Commands/downloadAttachedFiles.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\AttachedFile;
+use Exception;
+use Illuminate\Support\Facades\Storage;
+
+class downloadAttachedFiles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cowiki:download-attached-files';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $filesInDb = AttachedFile::limit(5)->get();
+
+        foreach($filesInDb as $file) {
+            $prefix_url = 'https://www.offene-werkstaetten.org/files/cowiki/';
+            $url = $prefix_url . rawurlencode($file->filename);
+            $filesystem_target_path = 'test_download/'.$file->filename;
+            try {
+                Storage::disk('local')->put($filesystem_target_path, file_get_contents($url));
+
+                $size = Storage::size($filesystem_target_path);
+                $mime = Storage::mimeType($filesystem_target_path);
+
+                $file->mime_type = $mime;
+                $file->filesize = $size;
+                $file->save();
+
+                $this->info('Download: ' . $filesystem_target_path . ' success. ( '. $this::formatBytes($size) . ' , ' . $mime . ')');
+            } catch (Exception $e) {
+                $this->error($e->getMessage());
+            }
+        }
+    }
+
+    public static function formatBytes($size, $precision = 2)
+    {
+        if ($size > 0) {
+            $size = (int) $size;
+            $base = log($size) / log(1024);
+            $suffixes = array(' bytes', ' KB', ' MB', ' GB', ' TB');
+            return round(pow(1024, $base - floor($base)), $precision) . $suffixes[floor($base)];
+        } else {
+            return $size;
+        }
+    }
+}
