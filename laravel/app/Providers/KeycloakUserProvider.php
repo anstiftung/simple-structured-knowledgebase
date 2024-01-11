ec1d8810092a1f70f30dab9c02e946784735be66
makakken <post@makakken.de>
2024-01-11 13:41:26 +0100

    add default role to user, you can set name in config

--- laravel/app/Providers/KeycloakUserProvider.php
+++ laravel/app/Providers/KeycloakUserProvider.php
@@ -11,6 +11,7 @@ class KeycloakUserProvider extends EloquentUserProvider
     {
         $user = User::where('email', $credentials['email'])->first();
         $searched_role = config('cowiki.kc_editor_role_mapping');
+        $default_role = config('cowiki.kc_default_role');
 
         $credentials['name'] = $token->preferred_username;
 
@@ -18,12 +19,17 @@ class KeycloakUserProvider extends EloquentUserProvider
             $user = User::create($credentials);
         }
 
-        // This is syncing the Role from keycloak
+        // If the user has the kc_editor_role_mapping coming from keycloak, it becomes editor
         if(in_array($searched_role, $token->realm_access->roles) && !$user->hasRole('editor')) {
             $user->assignRole('editor');
+            $user->removeRole('writer');
         } elseif (!in_array($searched_role, $token->realm_access->roles) && $user->hasRole('editor')) {
             $user->removeRole('editor');
+            $user->assignRole('writer');
         }
+
+        // If the current user has no role, it get's the default role
+        if(count($user->roles->pluck('name')) < 1) $user->assignRole($default_role);
 
         return $user;
     }
