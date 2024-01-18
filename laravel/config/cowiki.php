0f7e090549f061bbe888072b0348a6a8fc9a7f5c
friedrichweise <account@weise.io>
2024-01-18 16:48:35 +0100

    moves allowed file types and allowed file size to config

--- laravel/config/cowiki.php
+++ laravel/config/cowiki.php
@@ -5,4 +5,9 @@ return [
     'kc_editor_role_mapping' => env('KEYCLOAK_EDITOR_ROLE', 'cowiki-admin'),
     // If the user has no role from keycloak, which can be mapped, he gets the following role
     'kc_default_role' => 'writer',
+
+    // alowed file types for uploads: https://laravel.com/docs/10.x/validation#validating-files-file-types
+    'upload_file_types' => ['png', 'jpg', 'jpeg', 'svg', 'mp4', 'zip', 'tar.gz', 'pdf', 'doc', 'xls', 'csv', 'pdf', 'ai', 'indd', 'odt', 'ods', 'odp'],
+    // allowed filesite for uploads https://laravel.com/docs/10.x/validation#validating-files-file-sizes
+    'upload_file_size' => '10mb'
 ];
