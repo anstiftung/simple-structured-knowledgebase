c3d3e95aeb05bfba069315cf594a144173d32b7c
friedrichweise <account@weise.io>
2024-01-25 14:33:45 +0100

    removes tiptap starter kit and use Marks explicitly

--- vue/src/components/fields/Editor.vue
+++ vue/src/components/fields/Editor.vue
@@ -1,10 +1,22 @@
 <script setup>
 import { watch } from 'vue'
-import { useEditor, EditorContent } from '@tiptap/vue-3'
+
+import History from '@tiptap/extension-history'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import Heading from '@tiptap/extension-heading'
+import Bold from '@tiptap/extension-bold'
 import Underline from '@tiptap/extension-underline'
+import ListItem from '@tiptap/extension-list-item'
+import OrderedList from '@tiptap/extension-ordered-list'
+import BulletList from '@tiptap/extension-bullet-list'
+import Italic from '@tiptap/extension-italic'
+import Strike from '@tiptap/extension-strike'
 import Link from '@tiptap/extension-link'
-import StarterKit from '@tiptap/starter-kit'
 import Placeholder from '@tiptap/extension-placeholder'
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+
 import FloatingMenu from '@/components/fields/FloatingMenu.vue'
 import FixedMenu from '@/components/fields/FixedMenu.vue'
 
@@ -34,10 +46,18 @@ const ModelLink = Link.extend({
 
 const editor = useEditor({
   extensions: [
-    StarterKit.configure({
-      headings: { levels: [1, 2, 3, 4] },
-    }),
+    History,
+    Document,
+    Paragraph,
+    Text,
+    Heading.configure({ levels: [1, 2, 3, 4] }),
+    Bold,
     Underline,
+    BulletList,
+    OrderedList,
+    ListItem,
+    Italic,
+    Strike,
     ModelLink.configure({
       openOnClick: false,
     }),
