b5d6595dbf3dfb85eee7372946210c6337f9e73c
friedrichweise <account@weise.io>
2024-01-31 16:27:33 +0100

    adds sidebar to article.show

--- vue/src/views/Article/Show.vue
+++ vue/src/views/Article/Show.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import ArticleService from '@/services/ArticleService'
 import AttachmentCard from '@/components/AttachmentCard.vue'
-
+import ItemLine from '@/components/atoms/ItemLine.vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
@@ -37,17 +37,44 @@ loadFromServer()
         </div>
       </div>
     </section>
-    <section v-if="article" class="my-8 width-wrapper">
-      <h2>Inhalt</h2>
-      <div class="prose" v-html="article.content"></div>
-      <h2>Anhänge</h2>
-      <div class="grid grid-cols-3 gap-4">
-        <attachment-card
-          v-for="attachment in article.attached_urls.concat(
-            article.attached_files,
-          )"
-          :attachment="attachment"
-        />
+    <section v-if="article" class="grid grid-cols-6 my-8 width-wrapper">
+      <div class="col-span-4 px-8 py-16">
+        <div class="prose" v-html="article.content"></div>
+        <h2>Anhänge</h2>
+        <div class="grid grid-cols-3 gap-4">
+          <attachment-card
+            v-for="attachment in article.attached_urls.concat(
+              article.attached_files,
+            )"
+            :attachment="attachment"
+          />
+        </div>
+      </div>
+      <div class="self-start col-span-2 px-8 py-16 border-l">
+        <h4 class="mb-2 text-sm text-gray-300">Ersteller*in</h4>
+        <p>{{ article.created_by.name }}</p>
+        <div class="grid grid-cols-2 mt-8">
+          <div>
+            <h4 class="mb-2 text-sm text-gray-300">erstellt am</h4>
+            <p>{{ $filters.formatedDate(article.created_at) }}</p>
+          </div>
+          <div>
+            <h4 class="mb-2 text-sm text-gray-300">geändert am</h4>
+            <p>{{ $filters.formatedDate(article.updated_at) }}</p>
+          </div>
+        </div>
+        <h4 class="mt-8 mb-2 text-sm text-gray-300">
+          andere Sammlungen mit diesem Beitrag
+        </h4>
+        <p>
+          <item-line
+            v-for="collection in article.collections"
+            :navigate="true"
+            :showType="false"
+            :model="collection"
+            class="mb-2"
+          ></item-line>
+        </p>
       </div>
     </section>
   </div>
