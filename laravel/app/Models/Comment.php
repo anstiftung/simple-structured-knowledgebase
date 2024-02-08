561fdeb5717d8e779953c5544cb724c720272b9b
friedrichweise <account@weise.io>
2024-02-08 10:01:00 +0100

    adds softDelete to Comment Model

--- laravel/app/Models/Comment.php
+++ laravel/app/Models/Comment.php
@@ -6,10 +6,12 @@ use App\Models\Article;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\HasCreatedByAndUpdatedByTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     use HasCreatedByAndUpdatedByTrait;
 
     protected $fillable = [
