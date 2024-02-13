eb928318129dc8c4ef0a7ae3031df84807787ef7
friedrichweise <account@weise.io>
2024-02-13 07:26:20 +0100

    loads collection.articles in SearchController

--- laravel/app/Http/Controllers/SearchController.php
+++ laravel/app/Http/Controllers/SearchController.php
@@ -98,6 +98,8 @@ class SearchController extends Controller
             ->get();
         $numCollections = $collections->count();
 
+        $collections->load(['articles']);
+
         return [
             'data' => [ 'collections' => CollectionResource::collection($collections) ],
             'meta' => [ 'num_collections' => $numCollections ],
