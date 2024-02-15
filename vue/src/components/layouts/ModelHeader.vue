53ab8fd33ff75c24be73813b292504c670a395d9
friedrichweise <account@weise.io>
2024-02-15 10:05:34 +0100

    generalizes ModelHeader; adjusts placeholder text color

---
+++ vue/src/components/layouts/ModelHeader.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  colorClass: String,
+  secondaryColorClass: String,
+})
+</script>
+
+<template>
+  <section :class="secondaryColorClass">
+    <div :class="['header-clip', colorClass]">
+      <div class="py-12 text-center text-white width-wrapper">
+        <h3 class="mb-2 font-normal text-center opacity-70">
+          <slot name="description"></slot>
+        </h3>
+        <slot name="content"></slot>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.header-clip {
+  clip-path: polygon(0px 0px, 0 80%, 100% 100%, 100% 0px);
+}
+</style>
