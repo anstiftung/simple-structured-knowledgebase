3eef7327d66a239a08b2d0210ad1ef3589244613
makakken <post@makakken.de>
2024-02-26 08:39:13 +0100

    added two styles

--- vue/src/components/atoms/InputWithCounter.vue
+++ vue/src/components/atoms/InputWithCounter.vue
@@ -4,6 +4,13 @@ import { computed } from 'vue'
 const props = defineProps({
   modelValue: String,
   maxlength: Number,
+  position: {
+    type: String,
+    default: 'right',
+    validator(value, props) {
+      return ['bottom', 'right'].includes(value)
+    },
+  },
 })
 
 const emit = defineEmits(['update:modelValue'])
@@ -17,14 +24,20 @@ const remainingChars = computed(() => {
 })
 </script>
 <template>
-  <div>
+  <div class="relative">
     <input
       type="text"
       v-bind="$attrs"
       :value="modelValue"
       @input="updateValue"
     />
-    <p class="right-0 text-sm">
+    <p
+      class="text-sm"
+      :class="{
+        'absolute right-0': props.position == right,
+        'text-center mt-1': props.position == bottom,
+      }"
+    >
       {{ props.modelValue.length }} / {{ props.maxlength }}
     </p>
   </div>
