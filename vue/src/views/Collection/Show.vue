c50946e0b413cf70767bcfa56a7ecf6161c64fff
friedrichweise <account@weise.io>
2024-02-26 14:42:37 +0100

    adds Separator as own vue component

--- vue/src/views/Collection/Show.vue
+++ vue/src/views/Collection/Show.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import CollectionService from '@/services/CollectionService'
 import ArticleCard from '@/components/ArticleCard.vue'
 import ModelHeader from '@/components/layouts/ModelHeader.vue'
+import Separator from '@/components/layouts/Separator.vue'
 
 import { useRoute, useRouter } from 'vue-router'
 
@@ -39,16 +40,10 @@ loadFromServer()
     </model-header>
     <section v-if="collection?.articles" class="my-8 width-wrapper">
       <p class="my-12">{{ collection.description }}</p>
-      <div class="flex items-center gap-4 my-8">
-        <div class="border-b border-blue-600 border-dotted grow" />
-        <h4 class="text-xl text-blue-600">
-          {{
-            collection.articles.length ? collection.articles.length : 'Keine'
-          }}
-          {{ collection.articles.length == 1 ? 'Beitrag' : 'Beiträge' }}
-        </h4>
-        <div class="border-b border-blue-600 border-dotted grow" />
-      </div>
+      <separator>
+        {{ collection.articles.length ? collection.articles.length : 'Keine' }}
+        {{ collection.articles.length == 1 ? 'Beitrag' : 'Beiträge' }}
+      </separator>
       <div class="grid grid-cols-4 gap-4">
         <article-card
           v-for="article in collection.articles"
