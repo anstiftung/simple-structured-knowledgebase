3f6e20d9149d32adbb2787f93404c600b75c97d6
friedrichweise <account@weise.io>
2024-02-27 16:21:29 +0100

    adds headlessui selects and redesigns fixed menu

--- vue/src/components/editor/FixedMenu.vue
+++ vue/src/components/editor/FixedMenu.vue
@@ -3,6 +3,12 @@ import { ref, computed, onMounted } from 'vue'
 import { useModalStore } from '@/stores/modal'
 
 import ModelSelector from '@/components/atoms/ModelSelector.vue'
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue'
 
 const modal = useModalStore()
 
@@ -11,34 +17,49 @@ const props = defineProps({
 })
 
 /* dropdown with differenct node Styles */
-const selectedStyle = ref('p')
-const styleOptions = ref({
-  p: { label: 'Absatz' },
-  h2: { label: 'Ü2', level: 2 },
-  h3: { label: 'Ü3', level: 3 },
-  h4: { label: 'Ü4', level: 4 },
-})
+const selectedStyle = ref({ key: 'p', label: 'Absatz' })
+const styleOptions = ref([
+  { key: 'p', label: 'Absatz' },
+  { key: 'h2', label: 'Headline 1', level: 2 },
+  { key: 'h3', label: 'Headline 2', level: 3 },
+  { key: 'h4', label: 'Headline 3', level: 4 },
+])
 
 const onStyleInput = () => {
-  if (selectedStyle.value === 'p') {
+  if (selectedStyle.value.key === 'p') {
     props.editor.chain().focus().clearNodes().run()
   } else {
-    const level = styleOptions.value[selectedStyle.value].level
-    props.editor.chain().focus().setHeading({ level: level }).run()
+    props.editor
+      .chain()
+      .focus()
+      .setHeading({ level: selectedStyle.value.level })
+      .run()
   }
+}
+
+/* dropdown with different link options */
+const selectedLink = ref(null)
+const linkOptions = ref([
+  { key: 'collections', label: 'Sammlung' },
+  { key: 'articles', label: 'Beitrag' },
+  { key: 'attachments', label: 'Anhang' },
+])
+
+const onLinkInput = () => {
+  toggleLinkSelection(selectedLink.value.key)
 }
 
 /* dropdown with differenct Infoboxes */
 const selectedInfoBox = ref(null)
-const infoBoxOptions = ref({
-  warning: { label: 'Hinweis' },
-  danger: { label: 'Gefahrenhinweis' },
-  question: { label: 'Fragestellung' },
-})
+const infoBoxOptions = ref([
+  { key: 'warning', label: 'Hinweis' },
+  { key: 'danger', label: 'Gefahrenhinweis' },
+  { key: 'question', label: 'Fragestellung' },
+])
 
 const onInfoBoxInput = () => {
   props.editor.commands.setNode('infoBox', {
-    'data-type': selectedInfoBox.value,
+    'data-type': selectedInfoBox.value.key,
   })
 }
 /* allows toggling links to different model types */
@@ -50,7 +71,6 @@ const toggleLinkSelection = type => {
 
   // remove itemLink
   if (props.editor.isActive('itemLink')) {
-    let test = props.editor.getAttributes('itemLink')
     props.editor.commands.deleteNode('itemLink')
     props.editor.commands.insertContent(text)
     return
@@ -89,18 +109,6 @@ const toggleLinkSelection = type => {
   })
 }
 
-/* checks which of the above inserted links is currently active */
-const editorLinkActive = linkType => {
-  if (props.editor.isActive('itemLink') && props.editor.isFocused) {
-    let type = props.editor.getAttributes('itemLink')['data-type']
-    if (type == 'AttachedFile' || type == 'AttachedUrl') {
-      type = 'Attachment'
-    }
-    return type == linkType
-  }
-  return false
-}
-
 /* allows inserting attachments as inline images */
 const insertAttachmentAsImage = () => {
   modal.open(ModelSelector, { modelType: 'images' }, selection => {
@@ -120,20 +128,20 @@ onMounted(() => {
   props.editor.on('selectionUpdate', ({ editor }) => {
     // updates selectedStyle value
     if (editor.isActive('paragraph')) {
-      selectedStyle.value = 'p'
+      selectedStyle.value = styleOptions.value.find(o => o.key == 'p')
     }
     for (let index = 1; index <= 4; index++) {
       if (editor.isActive('heading', { level: index })) {
-        selectedStyle.value = `h${index}`
+        selectedStyle.value = styleOptions.value.find(o => o.key == `h${index}`)
       }
     }
     // updates selectedInfoBox value
     if (!editor.isActive('infoBox')) {
       selectedInfoBox.value = null
     } else {
-      Object.keys(infoBoxOptions.value).forEach(key => {
-        if (editor.isActive('infoBox', { 'data-type': key })) {
-          selectedInfoBox.value = key
+      infoBoxOptions.value.forEach(option => {
+        if (editor.isActive('infoBox', { 'data-type': option.key })) {
+          selectedInfoBox.value = option
         }
       })
     }
@@ -143,24 +151,33 @@ onMounted(() => {
 
 <template>
   <div
-    class="sticky z-20 inline-flex overflow-hidden bg-white border divide-x rounded-md top-header"
+    class="relative sticky z-20 inline-flex text-sm bg-white border divide-x rounded-md w-fit top-header"
     v-if="editor"
   >
-    <select
-      class="toolbarButton"
-      @change="onStyleInput"
-      v-model="selectedStyle"
-    >
-      <option v-for="(value, key) in styleOptions" :value="key">
-        {{ value.label }}
-      </option>
-    </select>
-
+    <div class="relative">
+      <Listbox v-model="selectedStyle" @update:model-value="onStyleInput">
+        <ListboxButton class="listbox-button" v-slot="{ open, value }"
+          >{{ value.label }}
+          <icon v-if="open" name="arrow-up"></icon>
+          <icon v-else name="arrow-down"></icon>
+        </ListboxButton>
+        <ListboxOptions class="listbox-options">
+          <ListboxOption
+            v-for="option in styleOptions"
+            :key="option.key"
+            :value="option"
+            class="listbox-option"
+          >
+            {{ option.label }}
+          </ListboxOption>
+        </ListboxOptions>
+      </Listbox>
+    </div>
     <button
       @click="editor.chain().focus().toggleBold().run()"
       :class="[
         ,
-        [editor.isActive('bold') ? 'bg-gray-100' : ''],
+        [editor.isActive('bold') ? 'text-black' : ' text-gray-400'],
         'toolbarButton',
       ]"
     >
@@ -169,7 +186,7 @@ onMounted(() => {
     <button
       @click="editor.chain().focus().toggleItalic().run()"
       :class="[
-        [editor.isActive('italic') ? 'bg-gray-100' : ''],
+        [editor.isActive('italic') ? 'text-black' : ' text-gray-400'],
         'toolbarButton',
       ]"
     >
@@ -178,7 +195,7 @@ onMounted(() => {
     <button
       @click="editor.chain().focus().toggleUnderline().run()"
       :class="[
-        [editor.isActive('underline') ? 'bg-gray-100' : ''],
+        [editor.isActive('underline') ? 'text-black' : ' text-gray-400'],
         'toolbarButton',
       ]"
     >
@@ -187,7 +204,7 @@ onMounted(() => {
     <button
       @click="editor.chain().focus().toggleStrike().run()"
       :class="[
-        [editor.isActive('strike') ? 'bg-gray-100' : ''],
+        [editor.isActive('strike') ? 'text-black' : ' text-gray-400'],
         'toolbarButton',
       ]"
     >
@@ -197,7 +214,7 @@ onMounted(() => {
     <button
       @click="editor.chain().focus().toggleBulletList().run()"
       :class="[
-        [editor.isActive('bulletList') ? 'bg-gray-100' : ''],
+        [editor.isActive('bulletList') ? 'text-black' : ' text-gray-400'],
         'toolbarButton',
       ]"
     >
@@ -206,60 +223,81 @@ onMounted(() => {
     <button
       @click="editor.chain().focus().toggleOrderedList().run()"
       :class="[
-        [editor.isActive('orderedList') ? 'bg-gray-100' : ''],
+        [editor.isActive('orderedList') ? 'text-black' : ' text-gray-400'],
         'toolbarButton',
       ]"
     >
       <icon name="orderedlist"></icon>
     </button>
 
-    <button
-      :class="[
-        [editorLinkActive('Article') ? 'bg-gray-100' : ''],
-        'toolbarButton',
-      ]"
-      @click="toggleLinkSelection('articles')"
-    >
-      Beitrag
-    </button>
-    <button
-      :class="[
-        [editorLinkActive('Collection') ? 'bg-gray-100' : ''],
-        'toolbarButton',
-      ]"
-      @click="toggleLinkSelection('collections')"
-    >
-      Sammlung
-    </button>
-    <button
-      @click="toggleLinkSelection('attachments')"
-      :class="[
-        [editorLinkActive('Attachment') ? 'bg-gray-100' : ''],
-        'toolbarButton',
-      ]"
-    >
-      <icon name="attachment"></icon>
-    </button>
+    <div class="relative">
+      <Listbox v-model="selectedLink" @update:model-value="onLinkInput">
+        <ListboxButton class="listbox-button" v-slot="{ open }"
+          ><icon class="text-gray-400 size-4" name="attachment"></icon>
+          <icon v-if="open" name="arrow-up"></icon>
+          <icon v-else name="arrow-down"></icon>
+        </ListboxButton>
+        <ListboxOptions class="listbox-options">
+          <ListboxOption
+            v-for="option in linkOptions"
+            :key="option.key"
+            :value="option"
+            class="listbox-option"
+          >
+            {{ option.label }}
+          </ListboxOption>
+        </ListboxOptions>
+      </Listbox>
+    </div>
     <button
       @click="insertAttachmentAsImage()"
       :class="[
-        [editor.isActive('image') ? 'bg-gray-100' : ''],
+        [editor.isActive('image') ? 'text-black' : ' text-gray-400'],
         'toolbarButton',
       ]"
     >
       <icon name="image"></icon>
     </button>
-    <select
-      class="toolbarButton"
-      @change="onInfoBoxInput"
-      v-model="selectedInfoBox"
-    >
-      <option disabled selected :value="null">InfoBox</option>
-      <option v-for="(value, key) in infoBoxOptions" :value="key">
-        {{ value.label }}
-      </option>
-    </select>
+
+    <div class="relative">
+      <Listbox v-model="selectedInfoBox" @update:model-value="onInfoBoxInput">
+        <ListboxButton class="listbox-button" v-slot="{ open }"
+          ><icon
+            :class="[
+              [selectedInfoBox ? 'text-black' : ' text-gray-400'],
+              'size-4',
+            ]"
+            :name="selectedInfoBox ? selectedInfoBox.key : 'warning'"
+          ></icon>
+          <icon v-if="open" name="arrow-up"></icon>
+          <icon v-else name="arrow-down"></icon>
+        </ListboxButton>
+        <ListboxOptions class="listbox-options">
+          <ListboxOption
+            v-for="option in infoBoxOptions"
+            :key="option.key"
+            :value="option"
+            class="listbox-option"
+          >
+            <icon :name="option.key"></icon> {{ option.label }}
+          </ListboxOption>
+        </ListboxOptions>
+      </Listbox>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.toolbarButton {
+  @apply px-3 py-2;
+}
+.listbox-button {
+  @apply relative inline-flex items-center h-full gap-2 text-left cursor-default toolbarButton;
+}
+.listbox-options {
+  @apply absolute overflow-auto text-gray-300 bg-white divide-y shadow-lg rounded-b-md;
+}
+.listbox-option {
+  @apply list-none cursor-pointer whitespace-nowrap toolbarButton hover:text-black;
+}
+</style>
