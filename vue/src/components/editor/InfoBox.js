ccca677be52702d9e9e84426b89229679d98d236
friedrichweise <account@weise.io>
2024-02-29 15:28:01 +0100

    proof of not work

--- vue/src/components/editor/InfoBox.js
+++ vue/src/components/editor/InfoBox.js
@@ -37,6 +37,7 @@ export default Node.create({
       // Markup is slightly different, than the output. Reason: tiptap requires two dom elements to be returned
       /*
         <div class="info-box">
+        <svg use="/icons/warning.svg#warning"/>
           <div class="content"></div>
         </div>
       */
@@ -46,8 +47,23 @@ export default Node.create({
       dom.classList.add('info-box')
       dom.dataset.type = node.attrs['data-type']
 
-      const content = document.createElement('div')
+      const iconSVG = document.createElementNS(
+        'http://www.w3.org/2000/svg',
+        'svg',
+      )
 
+      iconSVG.setAttribute('fill', 'currentColor')
+      iconSVG.setAttribute('viewBox', '0 0 15 15')
+      iconSVG.setAttribute('width', '15px')
+      iconSVG.setAttribute('height', '15px')
+
+      const useElement = document.createElementNS(null, 'use')
+      useElement.setAttribute('href', `/icons/danger.svg#danger`)
+
+      iconSVG.append(useElement)
+      dom.append(iconSVG)
+
+      const content = document.createElement('div')
       content.classList.add('content')
 
       dom.append(content)
