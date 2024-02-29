fc27da672112048ac9100a368820628da6e0df99
friedrichweise <account@weise.io>
2024-02-29 19:03:25 +0100

    updates Seeder to new infoBox structure; uses icons from "/icons/"-dir for InfoBox

--- vue/src/components/editor/InfoBox.js
+++ vue/src/components/editor/InfoBox.js
@@ -10,7 +10,7 @@ export default Node.create({
   addAttributes() {
     return {
       class: {
-        default: 'info-box text-black',
+        default: 'info-box',
       },
       'data-type': {
         default: 'warning', // ['warning', 'question', 'danger']
@@ -28,52 +28,50 @@ export default Node.create({
     ]
   },
 
-  renderHTML({ HTMLAttributes }) {
-    return ['div', mergeAttributes(HTMLAttributes), 0]
+  renderHTML({ HTMLAttributes, node }) {
+    const [dom, contentElement] = getMainDom(node)
+    return dom
   },
 
   addNodeView() {
     return ({ node }) => {
-      // Markup is slightly different, than the output. Reason: tiptap requires two dom elements to be returned
-      /*
-        <div class="info-box">
-        <svg use="/icons/warning.svg#warning"/>
-          <div class="content"></div>
-        </div>
-      */
-
-      const dom = document.createElement('div')
-
-      dom.classList.add('info-box')
-      dom.dataset.type = node.attrs['data-type']
-
-      const iconSVG = document.createElementNS(
-        'http://www.w3.org/2000/svg',
-        'svg',
-      )
-
-      iconSVG.setAttribute('viewBox', '0 0 15 15')
-      iconSVG.setAttribute('width', '15px')
-      iconSVG.setAttribute('height', '15px')
-
-      const useElement = document.createElementNS(
-        'http://www.w3.org/2000/svg',
-        'use',
-      )
-      useElement.setAttribute('href', `/icons/danger.svg#danger`)
-
-      iconSVG.append(useElement)
-      dom.append(iconSVG)
-
-      const content = document.createElement('div')
-      content.classList.add('content')
-
-      dom.append(content)
-
+      const [dom, contentElement] = getMainDom(node)
       return {
         dom,
-        contentDOM: content,
+        contentDOM: contentElement,
       }
     }
   },
 })
+
+function getMainDom(node) {
+  const dom = document.createElement('div')
+  dom.classList.add('info-box')
+  dom.dataset.type = node.attrs['data-type']
+
+  const iconSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+
+  iconSVG.setAttribute('viewBox', '0 0 15 15')
+  iconSVG.setAttribute('width', '15px')
+  iconSVG.setAttribute('height', '15px')
+
+  const useElement = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'use',
+  )
+  useElement.setAttribute(
+    'href',
+    `/icons/${node.attrs['data-type']}.svg#${node.attrs['data-type']}`,
+  )
+
+  iconSVG.append(useElement)
+  dom.append(iconSVG)
+
+  const contentElement = document.createElement('div')
+  contentElement.classList.add('content')
+  contentElement.innerText = node.textContent
+
+  dom.append(contentElement)
+
+  return [dom, contentElement]
+}
