645159ebaadb1c2228f3b6f1c8b592a591eba060
makakken <post@makakken.de>
2024-04-19 11:42:25 +0200

    adds security check in production mode

--- laravel/app/Console/Commands/MakeAllCollectionsPublished.php
+++ laravel/app/Console/Commands/MakeAllCollectionsPublished.php
@@ -4,15 +4,18 @@ namespace App\Console\Commands;
 
 use App\Models\Collection;
 use Illuminate\Console\Command;
+use Illuminate\Console\ConfirmableTrait;
 
 class MakeAllCollectionsPublished extends Command
 {
+    use ConfirmableTrait;
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'cowiki:make-all-collections-published';
+    protected $signature = 'cowiki:make-all-collections-published {--force : Force the operation to run when in production}';
 
     /**
      * The console command description.
@@ -26,6 +29,10 @@ class MakeAllCollectionsPublished extends Command
      */
     public function handle()
     {
+        if (! $this->confirmToProceed()) {
+            return 1;
+        }
+
         $collections = Collection::all();
         foreach ($collections as $collection) {
             $collection->published = true;
