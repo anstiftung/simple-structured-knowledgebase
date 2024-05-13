fe8b6e15f2fb9e23e2d7a888738d7312d42eb56f
makakken <post@makakken.de>
2024-05-13 22:06:49 +0200

    added new how-to-cowiki section

--- vue/src/components/layouts/ExplainSection.vue
+++ vue/src/components/layouts/ExplainSection.vue
@@ -1,20 +1,85 @@
+<script setup>
+import Separator from '@/components/layouts/Separator.vue'
+</script>
 <template>
-  <h3 class="mb-4 text-xl text-black">Wie funktioniert das COWIKI?</h3>
-  <div class="grid grid-cols-3 py-12 gap-x-24 gap-y-6">
-    <img class="place-self-center max-w-[150px]" src="/img/explain01.svg" />
-    <img class="place-self-center max-w-[150px]" src="/img/explain02.svg" />
-    <img class="place-self-center max-w-[150px]" src="/img/explain03.svg" />
-    <p class="justify-self-center max-w-[200px] text-center">
-      Das <span class="font-bold text-blue-600">COWIKI</span> besteht aus vielen
-      <span class="font-bold text-orange">Beiträgen</span>.
-    </p>
-    <p class="justify-self-center max-w-[200px] text-center">
-      Gute <span class="font-bold text-green">Anhänge</span> kommen immer wieder
-      in Beiträgen vor.
-    </p>
-    <p class="justify-self-center max-w-[200px] text-center">
-      Zur Orientierug bündeln wir Beiträge in thematischen
-      <span class="font-bold text-blue-400">Sammlungen</span>.
-    </p>
+  <div id="how-to-cowiki" class="mb-12">
+    <separator>
+      <h3>Wie funktioniert das COWIKI?</h3>
+    </separator>
+
+    <div class="grid grid-cols-2 py-12 gap-x-6 gap-y-6 w-9/12 mx-auto">
+      <router-link
+        to="/beitrag/was-ist-ein-cowiki-beitrag"
+        class="aspect-square border border-gray flex justify-center items-center transition-all hover:shadow-md"
+      >
+        <div class="text-center">
+          <img
+            class="max-w-[150px] inline-block mb-4"
+            src="/img/explain01.svg"
+          />
+          <p class="max-w-[200px]">
+            Das <span class="font-bold text-blue-600">COWIKI</span> besteht aus
+            vielen <span class="font-bold text-orange">Beiträgen</span>.
+          </p>
+        </div>
+      </router-link>
+
+      <router-link
+        to="/beitrag/geprufte-beitrage-beklatschen-und-kommentieren"
+        class="aspect-square border border-gray flex justify-center items-center transition-all hover:shadow-md"
+      >
+        <div class="text-center">
+          <img
+            class="max-w-[150px] inline-block mb-4"
+            src="/img/explain02.svg"
+          />
+          <p class="max-w-[200px]">
+            Geprüfte <span class="font-bold text-orange">Beiträge</span>?
+            <span class="font-bold text-blue-600">Beklatschen</span> und
+            <span class="font-bold text-blue-600">kommentieren</span>?
+          </p>
+        </div>
+      </router-link>
+
+      <router-link
+        to="/beitrag/was-ist-ein-cowiki-anhang"
+        class="aspect-square border border-gray flex justify-center items-center transition-all hover:shadow-md"
+      >
+        <div class="text-center">
+          <img
+            class="max-w-[150px] inline-block mb-4"
+            src="/img/explain03.svg"
+          />
+          <p class="max-w-[200px]">
+            Gute <span class="font-bold text-green">Anhänge</span> kommen immer
+            wieder in Beiträgen vor.
+          </p>
+        </div>
+      </router-link>
+
+      <router-link
+        to="/beitrag/was-ist-eine-cowiki-sammlung"
+        class="aspect-square border border-gray flex justify-center items-center transition-all hover:shadow-md"
+      >
+        <div class="text-center">
+          <img
+            class="max-w-[150px] inline-block mb-4"
+            src="/img/explain04.svg"
+          />
+          <p class="max-w-[200px]">
+            Zur Orientierug bündeln wir Beiträge in thematischen
+            <span class="font-bold text-blue-400">Sammlungen</span>.
+          </p>
+        </div>
+      </router-link>
+    </div>
+    <div class="flex justify-center">
+      <router-link
+        to="/beitrag/cowiki-introduction"
+        class="default-button !px-12"
+      >
+        zur Anleitung
+      </router-link>
+    </div>
   </div>
 </template>
