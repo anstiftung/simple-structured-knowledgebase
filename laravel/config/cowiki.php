278079ce3e8081371c7c4b03a8a214bed7d0a6fe
makakken <post@makakken.de>
2025-01-27 11:44:21 +0100

    added xlsx as allowed upload_file_type

--- laravel/config/cowiki.php
+++ laravel/config/cowiki.php
@@ -7,7 +7,7 @@ return [
     'kc_default_role' => 'writer',
 
     // alowed file types for uploads: https://laravel.com/docs/10.x/validation#validating-files-file-types
-    'upload_file_types' => ['png', 'jpg', 'jpeg', 'svg', 'mp4', 'zip', 'tar.gz', 'pdf', 'doc', 'xls', 'csv', 'pdf', 'ai', 'indd', 'odt', 'ods', 'odp'],
+    'upload_file_types' => ['png', 'jpg', 'jpeg', 'svg', 'mp4', 'zip', 'tar.gz', 'pdf', 'doc', 'xls', 'xlsx', 'csv', 'pdf', 'ai', 'indd', 'odt', 'ods', 'odp'],
     // allowed filesite for uploads https://laravel.com/docs/10.x/validation#validating-files-file-sizes
     'upload_file_size' => '10mb',
 
